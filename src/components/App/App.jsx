099b69ba50ff5ee138450214ef46bd528593551e
Jeannie Branstrator <jmbranstrator@gmail.com>
2022-06-10 09:19:33 -0500

    made changes to component structures

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import './App.css';
-// import Gallery
+// import Header 
+import Header from '../Header/Header'; 
+// import Gallery 
 import Gallery from '../Gallery/Gallery'; 
 
 function App() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <p>Gallery goes here</p>
-        
+        <Header />
         <Gallery />
       </div>
     );
