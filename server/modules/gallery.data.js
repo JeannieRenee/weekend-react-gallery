6bc38c97b80038fb626c62f437319a38fd9a74a2
Jeannie Branstrator <jmbranstrator@gmail.com>
2022-06-10 20:05:54 -0500

    created conditional rendering, tested and working properly

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,10 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 2, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 3, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 4, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 5, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 6, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: '111 Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/goat_small.jpg', description: '222 Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 3, path: 'images/goat_small.jpg', description: '333 Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 4, path: 'images/goat_small.jpg', description: '444 Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 5, path: 'images/goat_small.jpg', description: '555 Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 6, path: 'images/goat_small.jpg', description: '666 Photo of a goat taken at Glacier National Park.', likes: 0 }
 ];
 
 module.exports = galleryItems;
